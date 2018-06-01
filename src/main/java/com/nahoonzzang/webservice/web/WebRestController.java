ee6b33c81cf92b2a1c58d6a7466a181fe0ad1cc9
nahoonzzang <0828namjin@gmail.com>
2018-06-01 22:16:39 +0900

    프로젝트 생성

--- src/main/java/com/nahoonzzang/webservice/web/WebRestController.java
+++ src/main/java/com/nahoonzzang/webservice/web/WebRestController.java
@@ -1,13 +1,25 @@
 package com.nahoonzzang.webservice.web;
 
+import com.nahoonzzang.webservice.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
